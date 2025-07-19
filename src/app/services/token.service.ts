dca1b478af54fe77dd6a69b39341f10c3d3dd23f
advin <adithpsycho@gmail.com>
2025-07-19 22:32:46 +0530

    token service

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -1,9 +1,29 @@
 import { Injectable } from '@angular/core';
+import { Token } from '../types';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TokenService {
+  public userToken = new BehaviorSubject<string | null>(null);
 
-  constructor() { }
+  constructor() {}
+
+  setToken(token: Token) {
+    const { accessToken } = token;
+    this.userToken.next(accessToken);
+    localStorage.setItem(accessToken, 'accessToken');
+  }
+
+  fetchToken() {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      this.userToken.next(token);
+    }
+  }
+
+  clearToken() {
+    localStorage.removeItem('accessToken');
+  }
 }
