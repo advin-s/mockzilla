81afe84451e5f4e5af3ab94732cebbbb0a3c675e
advin <adithpsycho@gmail.com>
2025-07-20 15:01:09 +0530

    store token effect

--- src/app/store/auth/auth.effects.ts
+++ src/app/store/auth/auth.effects.ts
@@ -2,13 +2,15 @@ import { inject, Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
 import { AuthService } from '../../services/auth.service';
 import { login, loginSuccess, loginFailure } from './auth.actions';
-import { map, mergeMap, catchError, of } from 'rxjs';
+import { map, mergeMap, catchError, of, take, tap } from 'rxjs';
 import { AuthData } from '../../interfaces';
+import { TokenService } from '../../services/token.service';
 
 @Injectable()
 export class AuthEffects {
   private actions$ = inject(Actions);
   private authService = inject(AuthService);
+  private tokenService = inject(TokenService);
 
   login$ = createEffect(() =>
     this.actions$.pipe(
@@ -16,9 +18,21 @@ export class AuthEffects {
       mergeMap(({ loginData }) =>
         this.authService.onLogin(loginData).pipe(
           map((data: AuthData) => loginSuccess({ data })),
-          catchError(({error}) => of(loginFailure({ errorMessage: error })))
+          catchError(({ error }) => of(loginFailure({ errorMessage: error })))
         )
       )
     )
+  );
+
+  storeToken$ = createEffect(
+    () =>
+      this.actions$.pipe(
+        ofType(loginSuccess),
+        tap(({ data }) => {
+          const { accessToken } = data;
+          this.tokenService.setToken({ accessToken });
+        })
+      ),
+    { dispatch: false }
   );
 }
