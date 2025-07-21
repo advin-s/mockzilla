b909836a804c8ffcaf2388343b76b417523fc8ba
advin <adithpsycho@gmail.com>
2025-07-21 12:50:00 +0530

    added auth guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
     {
@@ -9,6 +10,7 @@ export const routes: Routes = [
     },
     {
         path:'dashboard',
+        canMatch:[authGuard],
         loadComponent : () => import('./dashboard/dashboard.component').then(c => c.DashboardComponent),
         title:'Welcome to mockzilla dashboard'
     }
--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,5 +1,19 @@
-import { CanMatchFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanMatchFn, Router } from '@angular/router';
+import { TokenService } from '../services/token.service';
+import { map } from 'rxjs';
 
 export const authGuard: CanMatchFn = (route, segments) => {
-  return true;
+  const tokenService = inject(TokenService);
+  const router = inject(Router);
+
+  return tokenService.userToken.pipe(
+    map((token: string | null) => {
+      if (token) {
+        return true;
+      } else {
+        return router.parseUrl('');
+      }
+    })
+  );
 };
