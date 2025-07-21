4579d72725b04fcdb07bff8d9066bdce77bb313a
advin <adithpsycho@gmail.com>
2025-07-21 13:32:52 +0530

    sidebar ui and ngb

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { SidebarComponent } from "../sidebar/sidebar.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, SidebarComponent, RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as bootstrap from 'bootstrap';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap'
+
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [NgbTooltipModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
-export class SidebarComponent {
+export class SidebarComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+
+  }
 
 }
