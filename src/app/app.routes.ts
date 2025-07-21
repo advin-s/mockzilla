bac094cfff767c9b7a83f855bc578c7e73b4ddad
advin <adithpsycho@gmail.com>
2025-07-21 13:52:36 +0530

    added products and recipes component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,36 @@ import { LoginComponent } from './login/login.component';
 import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
-    {
-        path:'',
-        component:LoginComponent,
-        title:'Login to Mockzilla'
-    },
-    {
-        path:'dashboard',
-        canMatch:[authGuard],
-        loadComponent : () => import('./dashboard/dashboard.component').then(c => c.DashboardComponent),
-        title:'Welcome to mockzilla dashboard'
-    }
+  {
+    path: '',
+    component: LoginComponent,
+    title: 'Login to Mockzilla',
+  },
+  {
+    path: 'dashboard',
+    canMatch: [authGuard],
+    loadComponent: () =>
+      import('./dashboard/dashboard.component').then(
+        (c) => c.DashboardComponent
+      ),
+    title: 'Welcome to mockzilla dashboard',
+    children: [
+      {
+        path: 'products',
+        loadComponent: () =>
+          import('./dashboard/products/products.component').then(
+            (c) => c.ProductsComponent
+          ),
+        title: 'Products',
+      },
+      {
+        path: 'recipes',
+        loadComponent: () =>
+          import('./dashboard/recipes/recipes.component').then(
+            (c) => c.RecipesComponent
+          ),
+        title: 'Recipes',
+      },
+    ],
+  },
 ];
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,11 +1,12 @@
 import { AfterViewInit, Component } from '@angular/core';
 import * as bootstrap from 'bootstrap';
 import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap'
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 
 @Component({
   selector: 'app-sidebar',
-  imports: [NgbTooltipModule],
+  imports: [NgbTooltipModule, RouterLink,RouterLinkActive],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
