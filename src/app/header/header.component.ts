a5fc3a02b455c78a8f6dfccafde62069decf6a14
advin <adithpsycho@gmail.com>
2025-07-25 15:42:19 +0530

    cart button

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import {
+  ActivatedRoute,
+  NavigationEnd,
+  Router,
+  RouterEvent,
+  Event,
+} from '@angular/router';
+
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-  constructor(private authService:AuthService){}
+export class HeaderComponent implements OnInit {
+  showCart = signal<boolean>(false)
+  constructor(private authService: AuthService, private router: Router) {}
 
-  logout(){
-    this.authService.onLogout()
+  ngOnInit(): void {
+    this.showCart.set(this.router.url.includes('recipes'))
+    this.router.events
+      .pipe(
+        filter((event: Event | RouterEvent) => event instanceof NavigationEnd)
+      )
+      .subscribe((event: NavigationEnd) => {
+        const {url} = event
+        this.showCart.set(url.includes('recipes'))
+        
+      });
+  }
+
+  logout() {
+    this.authService.onLogout();
   }
 }
