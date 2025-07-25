46bbd4b81d9ac5c626afc897827473b69fec7d44
advin <adithpsycho@gmail.com>
2025-07-25 17:14:39 +0530

    products header

--- src/app/dashboard/products/products.component.ts
+++ src/app/dashboard/products/products.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { NgbDropdown, NgbDropdownItem, NgbDropdownMenu, NgbDropdownToggle } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-products',
-  imports: [],
+  imports: [NgbDropdown,NgbDropdownMenu,NgbDropdownToggle,NgbDropdownItem],
   templateUrl: './products.component.html',
   styleUrl: './products.component.scss'
 })
